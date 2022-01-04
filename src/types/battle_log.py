51afdc79eaa4a8a6f0b5c2fb6410df03193d2a43
Alexandre Silva <alex.paivasilva@poli.ufrj.br>
2022-01-04 10:14:49 -0300

    ref: Reorganized BattleLog class

--- src/types/battle_log.py
+++ src/types/battle_log.py
@@ -7,6 +7,8 @@ from src.types.enums.game_types import GAME_TYPES
 from src.types.enums.battle_log_sections import BATTLE_LOG_SECTIONS
 from typing import Dict, List, Optional, Set, TypedDict
 
+
+
 class PlayerMapping(TypedDict):
   p1: Optional[str]
   p2: Optional[str]
@@ -26,9 +28,6 @@ class BattleLog():
     self.rated: bool = False
     self.current_section: BATTLE_LOG_SECTIONS = BATTLE_LOG_SECTIONS.GAME_PREVIEW
     self.current_turn: int = 1
-
-    def parse_player(player_name: str) -> str:
-      return player_name[:2]
 
     def update_battle_section(line: List[str]) -> None:
       if len(line) == 1:
@@ -78,42 +77,24 @@ class BattleLog():
       elif len(line) == 4 or len(line) == 5:
         if line[0] == 'switch':
           raw_player, pokemon_name = line[1].split(': ')
-          player = parse_player(raw_player)
-
-          if Pokemon(pokemon_name) not in self.teams[player]:
-            self.teams[player].add(Pokemon(pokemon_name))
-            Logger().debug(f'Adding {pokemon_name} to {player}\'s team')
+          player = self.parse_player(raw_player)
+          self.add_pokemon(pokemon_name, player)
         elif line[0] == '-ability':
           raw_player, pokemon_name = line[1].split(': ')
-          player = parse_player(raw_player)
-          ability = line[2]
-
-          pokemon = self.teams[player].get(Pokemon(pokemon_name))
-          if pokemon is None:
-            Logger().error(f'{pokemon_name} is not a part of {player}\'s team. Currently, it has:')
-            Logger().error(self.teams[player])
-            raise InvalidPokemon
-
-          pokemon.set_ability(ability)
-          Logger().debug(f'Setting {player}\'s ability to {ability}')
+          player = self.parse_player(raw_player)
+          self.set_pokemon_ability(pokemon_name, player, line[2])
         elif line[0] == 'move':
           raw_player, pokemon_name = line[1].split(': ')
-          player = parse_player(raw_player)
+          player = self.parse_player(raw_player)
           move = line[2]
-
-          pokemon = self.teams[player].get(Pokemon(pokemon_name))
-          if pokemon is None:
-            Logger().error(f'{pokemon_name} is not a part of {player}\'s team. Currently, it has:')
-            Logger().error(self.teams[player])
-            raise InvalidPokemon
-
-          pokemon.add_move(move)
-          Logger().debug(f'Adding move {move} to {player}\'s {pokemon_name}')
+          self.add_pokemon_move(pokemon_name, player, move)
       
       for index, message in enumerate(line):
         if '[from] ability' in message:
-          pokemon_name = line[index + 1].replace('[of] ', '')
-          # (to-do)
+          ability = message.split(': ')[1]
+          raw_player, pokemon_name = line[index + 1].split(': ')
+          player = self.parse_player(raw_player.replace('[of] ', ''))
+          self.set_pokemon_ability(pokemon_name, player, ability)
             
 
     handler_by_section = {
@@ -140,3 +121,32 @@ class BattleLog():
     # print(f'tier: {self.tier}')
     # print(f'rated: {self.rated}')
     # print(f'current_section: {self.current_section}')
+  
+  @staticmethod
+  def parse_player(player_name: str) -> str:
+    return player_name[:2]
+  
+  def add_pokemon(self, pokemon_name: str, player: str) -> None:
+    if Pokemon(pokemon_name) not in self.teams[player]:
+      self.teams[player].add(Pokemon(pokemon_name))
+      Logger().debug(f'Adding {pokemon_name} to {player}\'s team')
+
+  def set_pokemon_ability(self, pokemon_name: str, player: str, ability: str) -> None:
+    pokemon = self.teams[player].get(Pokemon(pokemon_name))
+    if pokemon is None:
+      Logger().error(f'{pokemon_name} is not a part of {player}\'s team. Currently, it has:')
+      Logger().error(self.teams[player])
+      raise InvalidPokemon
+
+    pokemon.set_ability(ability)
+    Logger().debug(f'Setting {player}\'s ability to {ability}')
+  
+  def add_pokemon_move(self, pokemon_name: str, player: str, move: str) -> None:
+    pokemon = self.teams[player].get(Pokemon(pokemon_name))
+    if pokemon is None:
+      Logger().error(f'{pokemon_name} is not a part of {player}\'s team. Currently, it has:')
+      Logger().error(self.teams[player])
+      raise InvalidPokemon
+
+    pokemon.add_move(move)
+    Logger().debug(f'Adding move {move} to {player}\'s {pokemon_name}')
